1bb2c7fc95d98318d3356fd790058d8f9e9bf220
unknown <szymonkowal019@gmail.com>
2020-04-21 21:47:33 +0200

    First commit

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.ViewModels;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployee();
+            return View(model);
+        }
+        public ViewResult Details(int id)
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(id),
+                PageTitle = "Employee Details"
+            };
+            
+            return View(homeDetailsViewModel);
+        }
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                //return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+            return View();
+        }
+    }
+}
---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Mary",Department=Dept.HR,Email="Mary123@gmail.com" },
+                new Employee() { Id = 2, Name = "Jane",Department=Dept.IT,Email="jane1231@wp.pl" },
+                new Employee() { Id = 3, Name = "Szymon",Department=Dept.Payroll,Email="szymko123@wp.pl" }
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;
+        }
+
+        public Employee Add(Employee employee)
+        {
+            employee.Id = _employeeList.Max(e => e.Id) + 1;
+            _employeeList.Add(employee);
+            return employee;
+        }
+    }
+}
